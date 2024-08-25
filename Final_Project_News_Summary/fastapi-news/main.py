3ac48a62817fbc77a3a9dc4ea612efc419d625ca
Raisa Ahmed <raisaahmed22@gmail.com>
2024-08-25 20:34:45 +0600

    Add files via upload

---
+++ Final_Project_News_Summary/fastapi-news/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import uvicorn
+
+from app.routers import news, summary
+
+
+app = FastAPI(
+    title="AI based News Summary API",
+    version="0.2",
+    description="This is the API documentation for News Summary generating by AI.",
+    
+    contact={
+        "name": "Team-1: Raisa Ahmed, Tasnia Fahrin, Efter Jahan",
+        "email": "raisaahmed22@gmail.com",
+    },
+   
+    redoc_url="/documentation",
+    docs_url="/api-endpoints",
+)
+
+app.include_router(news.router)
+app.include_router(summary.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the News Summary API"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
